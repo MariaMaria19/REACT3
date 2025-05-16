20bc48145d3f348f17ba99e948883fff45634315
MariaMaria19 <ya.anistrat2005@yandex.com>
2025-05-16 11:20:45 +0300

    Create body.js

---
+++ body.js
@@ -0,0 +1,14 @@
+<body>
+    <div class="App">
+        <header class="App-header">
+            <h1>Users Posts App</h1>
+            <p>Click on a user to see their posts</p>
+        </header>
+
+        <div class="container">
+            <div class="users-list" id="users-container"></div>
+            <div class="posts-container" id="posts-container">
+                <div class="no-posts">Select a user to view their posts</div>
+            </div>
+        </div>
+    </div>
